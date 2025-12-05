deb30970ad414a155eb859a2b8ec5c801563e89c
D1149311 <darcy.930722@gmail.com>
2025-12-05 10:27:11 +0800

    chore/test

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 int main() {
     printf("=== C Language Calculator ===\n");
     //Operations placeholders
+    int a,b;
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("Invalid input\n");
+        return 1;
+    }
+    printf("Result: %d\n", a - b);
     printf("System Check OK.\n");
     return 0;
 }
