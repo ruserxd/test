80136ae97e015be640abe813eca1a655580f09a1
ruserxd <zxcmomo0219@gmail.com>
2025-12-05 10:33:11 +0800

    renew test

--- test_runner.sh
+++ test_runner.sh
@@ -2,11 +2,11 @@
 echo " Running Build Test..."
 gcc main.c -o app
 if [ $? -eq 0 ]; then
-echo " Compilation Successful!"
-./ app
-echo " Execution Test Passed!"
+echo "Compilation Successful!"
+./app
+echo "Execution Test Passed!"
 exit 0
 else
-echo " Compilation Failed!"
+echo "Compilation Failed!"
 exit 1
 fi
